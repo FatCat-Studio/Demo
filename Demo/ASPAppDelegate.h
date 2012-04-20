cff3e4f57cfc920a84ddcedc600e57378e88b40b
ASPCartman <aspcartman@gmail.com>
2012-04-20 11:40:34 +0400

    Initial Commit

---
+++ Demo/ASPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASPAppDelegate.h
+//  Demo
+//
+//  Created by Руслан Федоров on 4/20/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Руслан Федоров on 4/20/12.
+//  Copyright (c) 2012 MIPT iLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASPAppDelegate class]));
+	}
+}
