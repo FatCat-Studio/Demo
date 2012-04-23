1fb54a95662ce4cc9660a299bbe4059360beb0e6
Timofey Korchagin <tkorchagin@gmail.com>
2012-04-24 02:02:41 +0400

    addMars
    and BlackJack

--- Demo/TKSecondViewController.m
+++ Demo/TKSecondViewController.m
@@ -19,34 +19,44 @@
 	//Тут лежит список имен файлов текстур
 	NSArray *pics;
 	ASPGLSprite *earth;
+    ASPGLSprite *mars;
 	ASPGLSprite *ball;
 }
 
 //Тут желательно прогрузить все текстуры и насоздавать спрайтов.
 #define XSPEED (rand()%600)
 #define YSPEED (rand()%200)
-#define GCONST 50000
+#define GCONSTEARTH 10000
+#define GCONSTMARS 12000
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	//Заполняем список
 	self.backgroundColor=GLKVector3Make(0.3, 0.4, 0.3);
 	pics = [NSArray arrayWithObjects:@"Space_Invaders_by_maleiva.png",@"spaceinvaders.png",@"tits.png",nil];
+    
+    mars = [ASPGLSprite spriteWithTextureName:@"ball.png" effect:self.effect];
+    //Выставляем параметры созданного спрайта
+    mars.velocity=GLKVector2Make(0,0);
+    mars.contentSize=CGSizeMake(120,120);
+    mars.position=GLKVector2Make(self.viewIOSize.width/2+30, self.viewIOSize.height/2-earth.contentSize.height/2+50);
+
 	
     earth = [ASPGLSprite spriteWithTextureName:@"ball.png" effect:self.effect];
     //Выставляем параметры созданного спрайта
     earth.velocity=GLKVector2Make(0,0);
-    earth.contentSize=CGSizeMake(150,150);
-    earth.position=GLKVector2Make(self.viewIOSize.width/2, self.viewIOSize.height/2-earth.contentSize.height/2);
+    earth.contentSize=CGSizeMake(70,70);
+    earth.position=GLKVector2Make(self.viewIOSize.width/2-30, self.viewIOSize.height/2-earth.contentSize.height/2-90);
 
     
-   ball = [ASPGLSprite spriteWithTextureName:@"ball.png" effect:self.effect];
+    ball = [ASPGLSprite spriteWithTextureName:@"ball.png" effect:self.effect];
 	//Выставляем параметры созданного спрайта
     ball.velocity=GLKVector2Make(0, 100);
-    ball.contentSize=CGSizeMake(70,70);
+    ball.contentSize=CGSizeMake(30,30);
     ball.position=GLKVector2Make(self.viewIOSize.width/2+100, 0);
 	
 	[self.sprites addObject:ball];
+    [self.sprites addObject:mars];
 	[self.sprites addObject:earth];
 }
 
@@ -62,21 +72,32 @@
 -(void) recalculateVelocityEarth:(ASPGLSprite*)sp {
     // Здесь мы рассчитываем новую скорость шарика под действием силы тяжести Земли
     
-    GLfloat dx=earth.position.x-sp.position.x; // Разность X координат
-    GLfloat dy=earth.position.x-sp.position.y; // разность Y координат
+    GLfloat dx=earth.position.x-sp.position.x-sp.contentSize.height/2; // Разность X координат
+    GLfloat dy=earth.position.y-sp.position.y; // разность Y координат
     GLKVector2 vect=GLKVector2Make(dx, dy); // Направление действия силы
     vect=GLKVector2Normalize(vect); // Привели к единичной длине
-    CGFloat acceleration = GCONST/(dx*dx + dy*dy); // Модуль силы
+    CGFloat acceleration = GCONSTEARTH/(dx*dx + dy*dy); // Модуль силы
     vect=GLKVector2MultiplyScalar(vect, acceleration); // Задали направление ускорения
     sp.velocity=GLKVector2Add(sp.velocity, vect); // Добавляем изменения
 }
+
+-(void) recalculateVelocityMars:(ASPGLSprite*)sp {
+    // Здесь мы рассчитываем новую скорость шарика под действием силы тяжести Марса
+    
+    GLfloat dx=mars.position.x-sp.position.x-sp.contentSize.height/2; // Разность X координат
+    GLfloat dy=mars.position.y-sp.position.y; // разность Y координат
+    GLKVector2 vect=GLKVector2Make(dx, dy); // Направление действия силы
+    vect=GLKVector2Normalize(vect); // Привели к единичной длине
+    CGFloat acceleration = GCONSTMARS/(dx*dx + dy*dy); // Модуль силы
+    vect=GLKVector2MultiplyScalar(vect, acceleration); // Задали направление ускорения
+    sp.velocity=GLKVector2Add(sp.velocity, vect); // Добавляем изменения
+}
+
 //Тут нужно очистить кэш и sprites
 - (void)viewDidUnload 
 {
     [super viewDidUnload];
     pics=nil;
-	[self.sprites removeObject:earth];
-	[self.sprites removeObject:ball];
 	//[ASPGLSprite clearTextureCache];
 }
 
@@ -91,22 +112,14 @@
 	}
 }
 
-- (ASPGLSprite *) makeRandomSprite{
-    //Вот так правильно создавать спрайты. rand%[pics count] обеспечивает рандомную текстуру
-    ASPGLSprite *sprite=[ASPGLSprite spriteWithTextureName:[pics objectAtIndex:rand()%[pics count]] effect:self.effect];
-    if (!sprite){
-        NSLog(@"Failed to create a sprite");
-        return nil;
-    }
-    return sprite;
-}
-
 //Этот метод дергается каждый раз, когда GLKit решит, что пора бы
 //пересчитать логику. Соответственно тут нужно раздать указания
 //sprite'ам, что им делать и дернуть у них update
 - (void)update{
+    
 	//Тут логика игры и раздача пиздюлей спрайтам.
     [self recalculateVelocityEarth:ball];
+    [self recalculateVelocityMars:ball];
     [self recalculateVelocityWalls:ball];
     [ball update:self.timeSinceLastUpdate];
 }
